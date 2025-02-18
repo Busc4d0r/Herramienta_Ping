63311dde2a024731c9c5b3f337748fcbb3a791b2
lramos <luyne.ld@outlook.com>
2025-02-18 19:40:17 -0400

    refactor: mejorar funciones de diagnóstico y agregar pruebas de velocidad de conexión

--- test_netw.py
+++ test_netw.py
@@ -3,14 +3,12 @@ import platform
 import os
 import speedtest
 
-#guardo las direcciones que necesito consultar
-ip_host1 = '192.168.1.1' #IP de DMP
-ip_dns = '94.140.14.14' #IP del DNS
-
-#con este codigo obtengo el nombre del DNS a partir de su direccion IP
-if ip_dns == '1.1.1.1' : nombre_dns = 'CloudFlare'
-elif ip_dns == '8.8.8.8' : nombre_dns = 'Google'
-elif ip_dns == '94.140.14.14' : nombre_dns = 'AdGuard'
+#direcciones ip de los hosts a los que hay que consultar
+ip_dm='192.168.1.1'
+ip_hex='192.168.254.1'
+ip_prov1='192.168.101.1'
+ip_prov2='172.21.156.2'
+ip_dns='8.8.8.8'
 
 #la siguiente funcion me permite limpiar la pantalla de la terminal
 def limpiar_pantalla():
@@ -19,30 +17,31 @@ def limpiar_pantalla():
     else:
         os.system('clear')
 
-#la siguiente funcion me permite hacer ping a la direccion IP del host 1
-def ping_host_1():
+# la siguiente función me permite testear la velocidad de conexión
+def test_speed():
     try:
-        if platform.system().lower() == 'windows':
-            comando = ['ping','-n','4',ip_host1]
-        else:
-            comando = ['ping','-c','4',ip_host1]
+        st = speedtest.Speedtest()
+        st.download()
+        st.upload()
+        st.results.share()
+        results = st.results.dict()
+        
+        print(f"    Velocidad de descarga: {results['download'] / 1_000_000:.2f} Mbps")
+        print(f"    Velocidad de subida: {results['upload'] / 1_000_000:.2f} Mbps")
+        print(f"    Ping: {results['ping']} ms")
+    except Exception as e:
+        print(f"Ocurrió un error al testear la velocidad de conexión: {e}")
 
-        resultado = subprocess.run(
-            comando,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            text=True
-        )
-        if resultado.returncode == 0:
-            print('    Ping exitoso, hay conexión con el host')
-            #print(resultado.stdout)
+#la siguiente funcion me permite trazar la ruta hacia la direccion IP del DNS
+def tracert_dns():
+    try:
+        comando = ['tracert', ip_dns] if subprocess.os.name == 'nt' else ['traceroute', ip_dns]
+        result = subprocess.run(comando, capture_output=True, text=True)
+        
+        if result.returncode == 0:
+            print(result.stdout)
         else:
-            print('    No se pudo hacer ping al host\n')
-            print('Consideraciones:')
-            print('    - Verifica el cable de red en la computadora')
-            print('    - Verifica si los equipos en el rack están encendidos')
-            #print(resultado.stderr)
-
+            print(f'    Error al ejecutar el comando: {result.stderr}')
     except Exception as e:
         print(f'Ocurrió un error: {e}')
 
@@ -61,90 +60,173 @@ def ping_dns():
             text=True
         )
         if resultado.returncode == 0:
-            print(f'    Ping exitoso, hay conexión con {nombre_dns}\n')
+            print(f'    Hay conexión a internet\n')
             #print(resultado.stdout)
         else:
-            print(f'    No se pudo hacer ping a {nombre_dns}\n')
+            print(f'    NO hay conexión a internet\n')
             #print(resultado.stderr)
     except Exception as e:
         print(f'Ocurrió un error: {e}')
 
-#la siguiente funcion me permite trazar la ruta hacia la direccion IP del DNS
-def tracert_dns():
+def ping_dm():
     try:
-        comando = ['tracert', ip_dns] if subprocess.os.name == 'nt' else ['traceroute', ip_dns]
-        result = subprocess.run(comando, capture_output=True, text=True)
-        
-        if result.returncode == 0:
-            print(result.stdout)
+        if platform.system().lower() == 'windows':
+            comando = ['ping','-n','4',ip_dm]
         else:
-            print(f'    Error al ejecutar el comando: {result.stderr}')
+            comando = ['ping','-c','4',ip_dm]
+
+        resultado = subprocess.run(
+            comando,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True
+        )
+        if resultado.returncode == 0:
+            print('    Hay conexión con el router Dream Machine\n')
+            #print(resultado.stdout)
+        else:
+            print('    NO hay conexión con el router Dream Machine\n')
+            #print(resultado.stderr)
+
     except Exception as e:
         print(f'Ocurrió un error: {e}')
 
-# la siguiente función me permite testear la velocidad de conexión
-def test_speed():
+def ping_prov1():
     try:
-        st = speedtest.Speedtest()
-        st.download()
-        st.upload()
-        st.results.share()
-        results = st.results.dict()
-        
-        print(f"    Velocidad de descarga: {results['download'] / 1_000_000:.2f} Mbps")
-        print(f"    Velocidad de subida: {results['upload'] / 1_000_000:.2f} Mbps")
-        print(f"    Ping: {results['ping']} ms")
+        if platform.system().lower() == 'windows':
+            comando = ['ping','-n','4',ip_prov1]
+        else:
+            comando = ['ping','-c','4',ip_prov1]
+
+        resultado = subprocess.run(
+            comando,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True
+        )
+        if resultado.returncode == 0:
+            print('    Hay conexión con Inter\n')
+            #print(resultado.stdout)
+        else:
+            print('    NO hay conexión con Inter\n')
+            #print(resultado.stderr)
+
     except Exception as e:
-        print(f"Ocurrió un error al testear la velocidad de conexión: {e}")
+        print(f'Ocurrió un error: {e}')
+
+def ping_prov2():
+    try:
+        if platform.system().lower() == 'windows':
+            comando = ['ping','-n','4',ip_prov2]
+        else:
+            comando = ['ping','-c','4',ip_prov2]
+
+        resultado = subprocess.run(
+            comando,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True
+        )
+        if resultado.returncode == 0:
+            print('    Hay conexión con Fibex\n')
+            #print(resultado.stdout)
+        else:
+            print('    NO hay conexión con Fibex\n')
+            #print(resultado.stderr)
+
+    except Exception as e:
+        print(f'Ocurrió un error: {e}')
 
 #la siguiente funcion me permite ejecutar un diagnostico automatico
 def diag_auto():
-    print(f'Verificando conexión con el host {ip_host1}')
-    ping_host_1()
-    print(f'Verificando conexión con {nombre_dns}')
+    print('verificando conexión con el router...')
+    ping_dm()
+    print('verificando conexión con Inter...')
+    ping_prov1()
+    print('verificando conexión con Fibex...')
+    ping_prov2()
+    print('verificando conexión con el DNS...')
     ping_dns()
+
+#opciones avanzadas
+def opc_av():
+    while True:
+        print('1: Verificar conexión con el switch')
+        print('2: Verificar conexión con el router Dream Machine')
+        print('3: Verificar conexión con Inter')
+        print('4: Verificar conexión con Fibex')
+        print('5: Verificar conexión con el DNS')
+        print('6: Trazar ruta hacia el DNS')
+        option=int(input('Ingrese el número de la acción correspondiente: '))
+        limpiar_pantalla()
+
+        if option == 1:
+            print('Verificando conexión con el switch...\n')
+            ping_switch()
+            input('Presione Enter para volver al menú ')
+            limpiar_pantalla()
+
+        elif option == 2:
+            print('Verificando conexión con el router Dream Machine...\n')
+            ping_dm()
+            input('Presione Enter para volver al menú ')
+            limpiar_pantalla()
+
+        elif option == 3:
+            print('Verificando conexión con Inter...\n')
+            ping_prov1()
+            input('Presione Enter para volver al menú ')
+            limpiar_pantalla()
+
+        elif option == 4:
+            print('Verificando conexión con Fibex...\n')
+            ping_prov2()
+            input('Presione Enter para volver al menú ')
+            limpiar_pantalla()
+
+        elif option == 5:
+            print('Verificando conexión con el DNS...\n')
+            ping_dns()
+            input('Presione Enter para volver al menú ')
+            limpiar_pantalla()
+
+        elif option == 6:
+            print('Trazando ruta hacia el DNS...\n')
+            tracert_dns()
+            input('Presione Enter para volver al menú ')
+            limpiar_pantalla()
+
+        elif option == 0:
+            break
+
+        else:
+            print('La opción no es válida')
 
 #la siguiente funcion me permite mostrar un menu con las opciones disponibles
 def menu():
     while True:
-        print('1: Verificar conexión a internet')
-        print(f'2: Trazar ruta hacia {nombre_dns}')
-        print('3: Diagnóstico automático')
-        print(f'4: Verificar conexion con el host {ip_host1}')
-        print('5: Testear velocidad de conexión')
+        print('1: Diagnóstico automático')
+        print('2: Prueba de velocidad')
+        print('3: Opciones avanzadas')
         print('0: Salir\n')
         opcion = int(input('Ingrese el número de la acción correspondiente: '))
         limpiar_pantalla()
 
 
         if opcion == 1:
-            print('Verificando conexión a internet...\n')
-            ping_dns()
-            input('Presione Enter para volver al menú ')
-            limpiar_pantalla()
-
-        elif opcion == 2:
-            print(f'Trazando ruta hacia {nombre_dns}\n')
-            tracert_dns()
-            input('Presione Enter para volver al menú ')
-            limpiar_pantalla()
-
-        elif opcion == 3:
             print('Ejecutando diagnóstico automático...\n')
             diag_auto()
             input('Presione Enter para volver al menú ')
             limpiar_pantalla()
 
-        elif  opcion == 4:
-            print(f'Verificando conexión con el host {ip_host1}\n')
-            ping_host_1()
+        elif opcion == 2:
+            print(f'Ejecutando prueba de velocidad...\n')
+            test_speed()
             input('Presione Enter para volver al menú ')
             limpiar_pantalla()
 
-        elif opcion == 5:
-            print('Testeando velocidad de conexión...\n')
-            test_speed()
-            input('Presione Enter para volver al menú ')
+        elif opcion == 3:
+            opc_av()
             limpiar_pantalla()
 
         elif opcion == 0:
